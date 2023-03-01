783a589b6831c2b3593585ed900098f9a373c67e
yeoheeju0 <LG@DESKTOP-35TD8TC>
2023-03-02 08:58:17 +0900

    YHJ 0302

--- momeal/src/main/java/co/doeat/management/controller/LikesController.java
+++ momeal/src/main/java/co/doeat/management/controller/LikesController.java
@@ -1,11 +1,17 @@
 package co.doeat.management.controller;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import co.doeat.management.service.LikesService;
+import co.doeat.management.service.LikesVO;
 
 @Controller
 public class LikesController {
@@ -13,7 +19,7 @@ public class LikesController {
 	@Autowired
 	private LikesService likesService;
 
-	// 마이페이지 - 좋아요 리스트 출력
+	// 마이페이지 - 식단 좋아요 리스트 출력
 	@RequestMapping("/myLikeList")
 	public String myLikeList(Model model) {
 		model.addAttribute("myPageLike", likesService.myLikeList());
@@ -21,7 +27,25 @@ public class LikesController {
 		return "myPages/myLikeList";
 	}
 
-	// 마이페이지 - 좋아요 상세
-	// @RequestMapping()
+	// 마이페이지 - 식단 좋아요 상세
+	@GetMapping("/myLikeList/{postNo}")
+	@ResponseBody
+	public Map<String, Object> likesSelect(@PathVariable int postNo) {
+		System.out.println(likesService.mylikeSelect(postNo));
+		return likesService.mylikeSelect(postNo);
+	}
+
+	// 마이페이지 - 챌린지 좋아요 리스트 출력
+	@RequestMapping("/myChallList")
+	public String myChallList(Model model) {
+		model.addAttribute("challLike", likesService.myChallList());
+		return "myPages/myChallList";
+	}
+
+	// 마이페이지 - 좋아요 삭제
+	@GetMapping("/myLikeDel")
+	public int myLikeDel(Model model, LikesVO vo) {
+		return likesService.myLikeDel(vo);
+	}
 
 }
--- momeal/src/main/java/co/doeat/management/service/LikesService.java
+++ momeal/src/main/java/co/doeat/management/service/LikesService.java
@@ -6,9 +6,15 @@ import java.util.Map;
 public interface LikesService {
 
 	// 마이페이지 - 좋아요 리스트 출력
-	public List<Map<String, Object>> myLikeList(); // 공동구매 전체리스트 조회
+	public List<Map<String, Object>> myLikeList();
 
 	// 마이페이지 - 좋아요 누른 게시글 상세내용
-	public Map<String, Object> mylikeSelect(String userId); // 공동구매 상세내역 단건조회
+	public Map<String, Object> mylikeSelect(int postNo);
+
+	// 마이페이지 - 챌린지 좋아요 리스트 출력
+	public List<Map<String, Object>> myChallList();
+
+	// 마이페이지 - 좋아요 삭제
+	public int myLikeDel(LikesVO vo);
 
 }
