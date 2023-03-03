72bfcdbf85c280ba21e07eba4df23aef39cf005b
jungeun <3whale3@gmail.com>
2023-03-03 17:53:05 +0900

    03.03 5:50pm JE

--- momeal/src/main/java/co/doeat/management/controller/ChallengeController.java
+++ momeal/src/main/java/co/doeat/management/controller/ChallengeController.java
@@ -3,7 +3,6 @@ package co.doeat.management.controller;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
