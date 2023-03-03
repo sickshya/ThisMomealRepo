e7b27b2ffe22a1ec195a5ec0d3c40b0c32139ff2
avoidAll <116549289+avoidAll@users.noreply.github.com>
2023-03-03 23:45:04 +0900

    Update ChallengeController.java
    sysout 제거 0303 23:44 JB

--- momeal/src/main/java/co/doeat/management/controller/ChallengeController.java
+++ momeal/src/main/java/co/doeat/management/controller/ChallengeController.java
@@ -29,7 +29,6 @@ public class ChallengeController {
   // 임시로 세션에 ID 값 담기
 		HttpSession session = request.getSession();
 		session.setAttribute("userId", "user3");
-		
 		String id = (String) session.getAttribute("userId");
 		
 		// 전체조회
@@ -37,9 +36,6 @@ public class ChallengeController {
 
 		// 인기순(좋아요 많은 순) 조회
 		model.addAttribute("challRec", challengeService.likeRankChallList(id));
-
-		System.out.println("====== 결과 ▶ " + session.getAttribute("userId"));
-		System.out.println("====== 인기순 조회 ▶ " + challengeService.likeRankChallList(id));
 
 		return "challenge/challenge";
 	}
@@ -50,9 +46,7 @@ public class ChallengeController {
 		HttpSession session = request.getSession();
 		map.put("userId", session.getAttribute("userId"));
 		map.put("no", no);
-		System.out.println("값 잘 들어갓나요...... " + map);
 		model.addAttribute("chall", challengeService.getChallenge(map));
-		System.out.println("====== 단건조회 ▶ " + challengeService.getChallenge(map));
 		return "challenge/challengeDetail";
 	}
 
@@ -64,7 +58,6 @@ public class ChallengeController {
 		// 세션에 담겨있는 아이디 값을 vo의 userId에 담기. (String) 처리 해줘야함
 		vo.setUserId((String) session.getAttribute("userId"));
 		challengeService.attendChall(vo);
-		
 		return "redirect:/myChallengeList";
 	}
 
@@ -74,40 +67,28 @@ public class ChallengeController {
 	public String myChallenge(Model model, Map<String, Object>map, HttpServletRequest request) {
 		HttpSession session = request.getSession();
 		map.put("userId", session.getAttribute("userId"));
-//		map.put("userId", "user3");
 		
 		model.addAttribute("myChall", challengeService.getMyChallList(map));
-		
-		System.out.println("====== 결과 ▶ " + challengeService.getMyChallList(map));
-		
 		return "challenge/myChallenge";
 	}
 	
 	// 진행중 - 전체조회(챌린지 참여신청 후 이동)
 	@RequestMapping("/myChallengeList")
 	public String myChallengeList(Model model, Map<String, Object> map, HttpServletRequest request) {
-
 		// 세션의 로그인 정보에 담겨있는 유저 본인의 아이디를 {key:"userId" value:세션 아이디} 이렇게 담아서 보내야함!!!
 		// 그럼 그 값을 읽어서 해당 유저의 정보만 보여줌
 		// 지금은 임시로 user3이라고 보내는중...
 		HttpSession session = request.getSession();
 		map.put("userId", session.getAttribute("userId"));
-
 //		map.put("userId", "user3");
 		model.addAttribute("myChall", challengeService.getMyChallList(map));
-
-		System.out.println("====== 결과 ▶ " + challengeService.getMyChallList(map));
-
 		return "challenge/myChallenge";
 	}
 
 	// 진행중 - 단건조회
 	@GetMapping("/myChallenge/{no}")
 	public String myChallengOne(Model model, @PathVariable int no) {
-
 		model.addAttribute("chall", challengeService.getMyChall(no));
-		System.out.println("====== 단건조회 ▶ " + challengeService.getMyChall(no));
-
 		return "challenge/myChallengeDetail";
 	}
 
