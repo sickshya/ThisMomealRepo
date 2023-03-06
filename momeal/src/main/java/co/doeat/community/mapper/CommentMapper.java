6db31d0e18bff37e0018c33fe5976e35a67779a2
Jo <Jo@DESKTOP-NI8KIGM>
2023-03-07 00:27:28 +0900

    03.07 12:27am JE

--- momeal/src/main/java/co/doeat/community/mapper/CommentMapper.java
+++ momeal/src/main/java/co/doeat/community/mapper/CommentMapper.java
@@ -6,4 +6,5 @@ import co.doeat.community.service.CommentVO;
 
 public interface CommentMapper {
 	public List<CommentVO> commentsList(CommentVO vo); // 댓글 조회
+	public void commentInsert(CommentVO vo); // 댓글 등록
 }
--- momeal/src/main/java/co/doeat/community/service/CommentService.java
+++ momeal/src/main/java/co/doeat/community/service/CommentService.java
@@ -4,4 +4,5 @@ import java.util.List;
 
 public interface CommentService {
 	public List<CommentVO> commentsList(CommentVO vo); // 댓글 조회
+	public void commentInsert(CommentVO vo); // 댓글 등록
 }
