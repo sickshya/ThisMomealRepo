f74a9373cd9a9d0eedbb195d570bfd389ba3940d
heeju <kimkaaa1206@naver.com>
2023-03-06 11:12:03 +0900

    YHJ 0306 11:11

--- momeal/src/main/java/co/doeat/community/mapper/CommunityMapper.java
+++ momeal/src/main/java/co/doeat/community/mapper/CommunityMapper.java
@@ -8,7 +8,6 @@ import co.doeat.activity.service.MealVO;
 public interface CommunityMapper {
 	
 	public List<Map<String, Object>> getCmntList(); // 전체조회
-	public MealVO getCommunity(int no); // 단건조회
-	// mealVO로 받으먼 안될거같기도 하고..syso 찍어서 필요한 컬럼 다 들어오는지 확인해보기! 안 되면 resultType="map"으로 바꿔야할지도..ㅎ
+	public MealVO getCommunity(Map<String, Object>map); // 단건조회
 	
 }
--- momeal/src/main/java/co/doeat/management/controller/LikesRestController.java
+++ momeal/src/main/java/co/doeat/management/controller/LikesRestController.java
@@ -29,6 +29,8 @@ public class LikesRestController {
 
 		vo.setUserId((String) session.getAttribute("userId"));
 		vo.setPostNo(postNo);
+
+		System.out.println(likesService.mylikeSelect(vo));
 		return likesService.mylikeSelect(vo);
 	}
 
@@ -37,20 +39,23 @@ public class LikesRestController {
 	public int myLikeDel(LikesVO vo, @PathVariable int no, HttpSession session, HttpServletRequest request) {
 		session = request.getSession();
 		session.setAttribute("userId", "user1");
+
 		vo.setNo(no);
 		return likesService.myLikeDel(vo);
 	}
 
 	// 마이페이지 - 챌린지 좋아요 추가
 	@PostMapping("/insertLikeChall/{postNo}")
-	public int insertLikeChall(LikesVO vo, @PathVariable int postNo, HttpSession session, HttpServletRequest request) {
+	public LikesVO insertLikeChall(LikesVO vo, @PathVariable int postNo, HttpSession session,
+			HttpServletRequest request) {
 		session = request.getSession();
 		session.setAttribute("userId", "user3");
 
 		vo.setUserId((String) session.getAttribute("userId"));
 		vo.setPostNo(postNo);
 		vo.setBoardCode("CT01");
-		return likesService.insertLikeChall(vo);
+		likesService.insertLikeChall(vo);
+		return vo;
 	}
 
 //	// 마이페이지 - 챌린지 좋아요 취소
