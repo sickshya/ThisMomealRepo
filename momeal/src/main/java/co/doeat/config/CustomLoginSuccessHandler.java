3d334a3d6440dd69ace815efa4a2be3456e3fdea
kingB <jeong03927@gmail.com>
2023-03-08 10:36:36 +0900

    0308 10:30 before conflict... TT

--- momeal/src/main/java/co/doeat/config/CustomLoginSuccessHandler.java
+++ momeal/src/main/java/co/doeat/config/CustomLoginSuccessHandler.java
@@ -1,22 +1,46 @@
-//package co.doeat.config;
-//
-//import java.io.IOException;
-//
-//import javax.servlet.ServletException;
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpServletResponse;
-//import javax.servlet.http.HttpSession;
-//
-//import org.springframework.security.core.Authentication;
-//import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-//
-//public class CustomLoginSuccessHandler implements AuthenticationSuccessHandler {
-//
-//	@Override
-//	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
-//			Authentication authentication) throws IOException, ServletException {
-//		HttpSession session = request.getSession();
-//		session.setAttribute("userId", request.getu);
-//		response.sendRedirect("/admin/home");
-//	}
-//}
+package co.doeat.config;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
+public class CustomLoginSuccessHandler implements AuthenticationSuccessHandler {
+
+	@Override
+	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+			Authentication authentication) throws IOException, ServletException {
+		HttpSession session = request.getSession();
+		
+		log.warn("Login Success");
+		List<String> roleNames = new ArrayList<>();
+
+		authentication.getAuthorities().forEach(authority -> {
+			roleNames.add(authority.getAuthority());
+		});
+		
+		log.warn("ROLE NAMES : " + roleNames);
+
+		if (roleNames.contains("ROLE_ADMIN")) {
+			response.sendRedirect("admin/adminChallenge");
+			return;
+		}
+		
+		if (roleNames.contains("ROLE_USER")) {
+			response.sendRedirect("/home");
+			return;
+		}
+		response.sendRedirect("/");
+	}
+
+}
