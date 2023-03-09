346ab450896ebe025e3cece24f943c8cc6cd96f0
heeju <kimkaaa1206@naver.com>
2023-03-09 18:25:00 +0900

    YHJ 0309 18:24

--- momeal/src/main/java/co/doeat/community/controller/GroupsRestController.java
+++ momeal/src/main/java/co/doeat/community/controller/GroupsRestController.java
@@ -1,15 +1,13 @@
 package co.doeat.community.controller;
 
 import java.util.List;
-import java.util.Map;
 
-import javax.servlet.ServletContext;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,15 +21,40 @@ public class GroupsRestController {
 
 	// 지정한 그룹 날짜에 따른 피드 불러오기
 	@RequestMapping("/groupsFeed/{postDate}/{no}")
-	public List<GroupsVO> myFeedList(Model model, 
-			                         @PathVariable String postDate, 
-			                         @PathVariable int no, 
-			                         HttpSession session) {
-		
+	public List<GroupsVO> myFeedList(Model model, @PathVariable String postDate, @PathVariable int no,
+			HttpSession session) {
+
 //		session.setAttribute("userId", "user1");
 //		String userId = (String) session.getAttribute("userId");
-		
-		return groupsService.groupsList(postDate, no);
+
+		return groupsService.grpSelect(postDate, no);
 	}
 
+	// 그룹리스트에서 모달로 멤버리스트 보여줌
+	@RequestMapping("/groupsList/{no}")
+	public List<GroupsVO> memList(@PathVariable int no) {
+		return groupsService.membList(no);
+	}
+
+	// 참여중인 그룹의 멤버탈퇴
+	@PostMapping("/grpMembDel/{userId}/{grpNo}")
+	public int grpMembDel(@PathVariable String userId, @PathVariable int grpNo) {
+		return groupsService.grpMembDel(userId, grpNo);
+	}
+
+	// 내가 만든 그룹 삭제
+	@PostMapping("/grpDel/{no}")
+	public int grpDel(@PathVariable int no) {
+		return groupsService.grpDel(no);
+	}
+
+	// 그룹 만들기
+	@PostMapping("/grpInsert")
+	public GroupsVO grpInsert(GroupsVO vo, HttpSession session) {
+		String userId = (String) session.getAttribute("userId");
+		vo.setUserId(userId);
+
+		groupsService.grpInsert(vo);
+		return vo;
+	}
 }
--- momeal/src/main/java/co/doeat/community/mapper/GroupsMapper.java
+++ momeal/src/main/java/co/doeat/community/mapper/GroupsMapper.java
@@ -8,12 +8,19 @@ import co.doeat.community.service.GroupsVO;
 
 public interface GroupsMapper {
 
-	public List<GroupsVO> groupsList(String postDate, int no); // 그룹 전체 리스트
+	public List<GroupsVO> grpSelect(String postDate, int no); // 그룹 단건조회
 
-	public Map<String, Object> groupsSelect(String userId); // 그룹 단건조회
-	
-	public List<GroupsVO> grpList(GroupsVO vo); // 참여중인 그룹
+	public Map<String, Object> grpMembSelect(String userId); // 그룹 멤버 단건조회
+
+	public List<GroupsVO> grpAllList(GroupsVO vo); // 참여중인 그룹 전체리스트
 
 	public List<GroupsVO> membList(int no); // 해당그룹 멤버리스트 조회
 
+	public int grpMembDel(String userId, int grpNo); // 참여중인 그룹의 멤버탈퇴
+
+	public int grpDel(int no); // 내가 만든 그룹 삭제
+
+	public int grpInsert(GroupsVO vo); // 그룹 만들기
+	
+	public int membInsert(GroupsVO vo); // 멤버 추가
 }
