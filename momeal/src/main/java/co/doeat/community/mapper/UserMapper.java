9732dc41b7f98790ff520795f069b4e0ef62a732
tnstl <tnstl@DESKTOP-0Q55I9Q>
2023-03-10 21:46:04 +0900

    20230310 21:45 KSS

--- momeal/src/main/java/co/doeat/community/mapper/UserMapper.java
+++ momeal/src/main/java/co/doeat/community/mapper/UserMapper.java
@@ -4,9 +4,8 @@ import java.util.List;
 
 import co.doeat.community.service.UserSearchVO;
 import co.doeat.community.service.UsersVO;
-import co.doeat.management.service.GroupPurchaseListVO;
 
-public interfssssssssss
+public interface UserMapper {
 	boolean isIdCheck(String id); // 아이디 중복 체크
 
 	// 유저
@@ -34,8 +33,6 @@ public interfssssssssss
 	// 출석체크 포인트 업데이트
 	int updateATPoint(UsersVO uvo);
 
-	// 체험단
-	int selectExpUser(UsersVO vo); //유저정보 select
 	
 	//myFeed회원정보
 	UsersVO myFeedUserSelect(String id);
--- momeal/src/main/java/co/doeat/management/controller/ExperienceController.java
+++ momeal/src/main/java/co/doeat/management/controller/ExperienceController.java
@@ -1,7 +1,5 @@
 package co.doeat.management.controller;
 
-import java.util.List;
-
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -12,9 +10,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import co.doeat.community.service.UserService;
 import co.doeat.community.service.UsersVO;
@@ -27,13 +24,13 @@ public class ExperienceController {
 
 	@Autowired
 	private ExperienceService experienceService;
-	
+
 	@Autowired
 	ServletContext servletContext;
-	
+
 	@Autowired
 	private UserService userService;
-	
+
 	@Value("${momeal.saveImg}")
 	private String saveImg;
 
@@ -46,38 +43,45 @@ public class ExperienceController {
 		model.addAttribute("ExpingList", experienceService.getExperiencing()); // 진행
 		return "experience/experienceList";
 	}
-	
+
 	// 체험단(단건조회)
 	@GetMapping("/experienceDetail/{no}")
 	public String experienceDetail(Model model, @PathVariable int no, HttpSession session, ExperienceVO evo) {
 		String id = (String) session.getAttribute("userId");
 		model.addAttribute("ExpOne", experienceService.ExperOne(no));
-	    model.addAttribute("no", no);
+		model.addAttribute("no", no);
 		return "experience/experienceDetail";
 	}
-	
-	// 체험단(호출)
-	@RequestMapping("/expr/exprFrm")
-	public String experience(UsersVO vo) {
-		userService.ExpSelectUser(vo);
+
+	// 체험단(호출) // select 하는곳 조회먼저.
+	@RequestMapping("/expr/exprFrm/{no}")
+	public String experience(HttpSession session, @PathVariable int no, Model model) {
+		String userId = (String) session.getAttribute("userId");
+		model.addAttribute("expSelect", experienceService.ExperOne(no));
+		model.addAttribute("userInfo", userService.userSelect(userId));
+		System.out.println(no + "체험단정보" + userId + "유저정보");
 		return "experience/experienceForm";
 	}
-	
-	// 체험단(신청)
-	@GetMapping("/expr/exprAply.do")
-	public String exprAply(HttpServletRequest request,
-								 Model model,
-								 ExperienceVO vo,
-								 UsersVO uvo
-								 ) {
-		//HttpSession session = request.getSession();
-		//String id = (String) session.getAttribute("userId");
-		model.addAttribute("userInfo",userService.ExpSelectUser(uvo));
-		model.addAttribute("expInfo", experienceService.ExpInsert(vo));
-		return "redirect:/expOrderList";
+
+	// 체험단(신청) // insert 하는곳 값 불러와야함.
+	@RequestMapping("/expr/exprAply.do")
+	@ResponseBody
+	public String exprAply(HttpSession session, Model model, ExprParticipantsVO pvo, ExperienceVO evo) {
+		String userId = (String) session.getAttribute("userId");
+		experienceService.expInsert(pvo);
+
+		return "신청이 완료되었습니다.";
 	}
-	
-	
+
+	// 체험단(조회)
+	@RequestMapping("/expOrderList")
+	public String expOrderList(HttpServletRequest request, ExprParticipantsVO vo, Model model, ExperienceVO evo) {
+		HttpSession session = request.getSession();
+		session.getAttribute("userId");
+		//model.addAttribute("expOrderList", experienceService.expOrderList());
+		return "purchase/pchOrderList";
+	}
+
 	// 관리자 ++++++++++++++++++++++++++++++++++++++++++++++++++++++
 	@RequestMapping("adminExperienceGroup")
 	public String adminExperienceGroup(Model model) {
