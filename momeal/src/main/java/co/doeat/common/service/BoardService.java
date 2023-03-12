73ad98f1a6eac5f2a312990ddc1e4d4d00d19b7c
yeoheeju0 <LG@DESKTOP-35TD8TC>
2023-03-13 02:17:23 +0900

    YHJ 0313 13:00

--- momeal/src/main/java/co/doeat/common/service/BoardService.java
+++ momeal/src/main/java/co/doeat/common/service/BoardService.java
@@ -1,4 +1,16 @@
 package co.doeat.common.service;
 
+import java.util.List;
+
 public interface BoardService {
+
+	List<BoardVO> faqList(); // faq 리스트
+
+	BoardVO faqSelect(BoardVO vo); // faq 상세
+
+	int faqInsert(BoardVO vo); // faq 추가
+
+	int faqUpdate(BoardVO vo);// faq 수정
+
+	int faqDelete(BoardVO vo); // faq 삭제
 }
--- momeal/src/main/java/co/doeat/common/service/BoardVO.java
+++ momeal/src/main/java/co/doeat/common/service/BoardVO.java
@@ -1,8 +1,23 @@
 package co.doeat.common.service;
 
+import java.util.Date;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import lombok.Data;
 
 @Data
 public class BoardVO {
+	private int no;
+	private String boardTitle;
+	private String boardSubject;
+	@DateTimeFormat(pattern = "yyyyMMdd")
+	@JsonFormat(pattern = "yyyyMMdd")
+	private Date boardDate;
+	private int boardHit;
+	private String boardCode;
+	private String userId;
 
 }
