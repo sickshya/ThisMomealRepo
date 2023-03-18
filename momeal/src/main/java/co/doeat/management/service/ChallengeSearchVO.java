ff8d2f067bdf32799b78e256b79fb3bb76ace049
mgyeo <mgyeo@DESKTOP-OAQRT6U>
2023-03-18 22:32:07 +0900

    20230318 MGM 22:32

--- momeal/src/main/java/co/doeat/management/service/ChallengeSearchVO.java
+++ momeal/src/main/java/co/doeat/management/service/ChallengeSearchVO.java
@@ -1,5 +1,7 @@
 package co.doeat.management.service;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
 import lombok.Data;
 
 @Data
@@ -7,8 +9,10 @@ public class ChallengeSearchVO extends ChallengeVO {
 	int first;
 	int last;
 	String keyword; // 검색 내용
+	@DateTimeFormat(pattern="yyyy-MM-dd")
 	String datekeyword1; // 검색 내용
 	String typeArr;
 	String type; // 검색 타입
+	@DateTimeFormat(pattern="yyyy-MM-dd")
 	String datekeyword2; // 검색 내용
 }
