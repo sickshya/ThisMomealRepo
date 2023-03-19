a786f24425a785282f5e18b241c4f42cfb7e62a9
ksdoll <ksdoll@DESKTOP-Q37AAA0>
2023-03-19 15:49:12 +0900

    0319 15:50 JB

---
+++ momeal/src/main/java/co/doeat/config/security/CustomErrorController.java
@@ -0,0 +1,24 @@
+package co.doeat.config.security;
+
+import java.net.http.HttpRequest;
+
+import javax.servlet.RequestDispatcher;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class CustomErrorController implements ErrorController {
+
+//	@RequestMapping("/error")
+//	public String error(HttpRequest request) {
+//		Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+//		return "error";
+//	}
+	
+	@RequestMapping("/error")
+	public String error() {
+		return "error/error";
+	}
+}
--- momeal/src/main/java/co/doeat/config/security/WebSecurityConfig.java
+++ momeal/src/main/java/co/doeat/config/security/WebSecurityConfig.java
@@ -35,7 +35,9 @@ public class WebSecurityConfig {
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http.authorizeHttpRequests((requests) -> requests
-				.antMatchers("/", "/home", "/login*", "/logout", "/signup/**", "/contentsMain/**", "/contentsDetail/**")
+				.antMatchers(
+						"/", "/home", "/login*", "/logout*", "/error", 
+						"/signup/**", "/contentsMain/**", "/contentsDetail/**")
 				.permitAll()
 				// ▲ 비로그인 유저까지 허가되는 접근 url
 				.antMatchers("/usrs/**", "/chlg/**", "/pch/**", "/exp/**", "/fllw/**", "/cmt/**", "/cty/**")
