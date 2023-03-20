a09535a0e5e563e290004fbb228507976f727d8c
avoidAll <116549289+avoidAll@users.noreply.github.com>
2023-03-20 10:34:14 +0900

    Update WebConfig.java

--- momeal/src/main/java/co/doeat/config/WebConfig.java
+++ momeal/src/main/java/co/doeat/config/WebConfig.java
@@ -18,7 +18,7 @@ public class WebConfig implements WebMvcConfigurer {
 	}
 
 	public void addViewControllers(ViewControllerRegistry registry) {
-		registry.addViewController("/").setViewName("/users/loginFrm");
+		registry.addViewController("/").setViewName("/base/home");
 		registry.addViewController("/home").setViewName("/base/home");
 		registry.addViewController("/main").setViewName("/base/home");
 		
