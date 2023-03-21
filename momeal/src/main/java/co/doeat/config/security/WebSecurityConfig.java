8ca4502e5c2d07ca06762c51736dcd62a5cfd089
kingB <jeong03927@gmail.com>
2023-03-21 14:50:12 +0900

    0321 14:50 JB

--- momeal/src/main/java/co/doeat/config/security/WebSecurityConfig.java
+++ momeal/src/main/java/co/doeat/config/security/WebSecurityConfig.java
@@ -35,9 +35,9 @@ public class WebSecurityConfig {
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http.authorizeHttpRequests((requests) -> requests
-				.antMatchers(
-						"/", "/home", "/login*", "/logout*", "/error", 
-						"/signup/**", "/contentsMain/**", "/contentsDetail/**")
+				.antMatchers("/", "/index*", "/home", "/main", 
+						"/login*", "/logout*", "/error", "/signup/**", 
+						"/contentsMain/**", "/contentsDetail/**")
 				.permitAll()
 				// ▲ 비로그인 유저까지 허가되는 접근 url
 				.antMatchers("/usr/**", "/chlg/**", "/pch/**", "/exp/**", "/fllw/**", "/cmt/**", "/cty/**")
@@ -51,6 +51,7 @@ public class WebSecurityConfig {
 						.loginProcessingUrl("/doLogin") // 로그인동작 시 매핑되는 url
 						.usernameParameter("userId") // 로그인 view 내의 form 태그 내에 로그인 할 때 username에 매핑되는 태그의 name
 						.passwordParameter("password") // 로그인 view 내의 form 태그 내에 로그인 할 때 password에 매핑되는 태그의 name
+						.defaultSuccessUrl("/home")
 						.successHandler(new CustomLoginSuccessHandler()) // 로그인 성공시 실행되는 핸들러
 						.failureUrl("/login?error") // 로그인 실패시 url
 						.failureHandler(new CustomLoginFailureHandler()) // 로그인 실패시 실행되는 핸들러
@@ -72,7 +73,9 @@ public class WebSecurityConfig {
 	public WebSecurityCustomizer webSecurityCustomizer() {
 		// Spring Security를 적용하지 않을 리소스를 설정
 		return web -> {
-			web.ignoring().antMatchers("/css/**", "/assets/**", "/data/**", "/fonts/**", "/js/**", "/massets/**",
+			web.ignoring()
+			.antMatchers(
+					"/css/**", "/assets/**", "/data/**", "/fonts/**", "/js/**", "/massets/**",
 					"/modal-template/**", "/second-template/**");
 		};
 	}
